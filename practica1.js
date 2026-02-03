b69f6cb6c7fc1beab59b382c856782e0d863feef
gabbr11 <pedro.sanxe08@gmail.com>
2026-02-03 10:43:12 +0100

    first commit

---
+++ practica1.js
@@ -0,0 +1,11 @@
+let nombre = "Gabriel";
+let edad = 20;
+let esAlumno = true;
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Es alumno:", esAlumno);
+
+console.log("Tipo de 'nombre':", typeof nombre);
+console.log("Tipo de 'edad':", typeof edad);
+console.log("Tipo de 'esAlumno':", typeof esAlumno);
